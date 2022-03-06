7e1053ab0c0e474938b3100a2e68e9bcfef0deac
Kunal3723 <78958213+Kunal3723@users.noreply.github.com>
2022-03-06 18:25:06 +0530

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import translateRoutes from "./routes/translateRoute.js";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
@@ -14,4 +15,4 @@ app.use('/translate', translateRoutes);
 
 app.get('/', function (req, res) {
     res.send("Welcome to REST Api");
-}).listen(5000, () => console.log(`Server Running on Port: http://localhost:5000`));
+}).listen(PORT, () => console.log(`Server Running on Port: http://localhost:5000`));
